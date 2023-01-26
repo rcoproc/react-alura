444f9a898e23241adb895b19423ebdeb079811c3
Ricardo César de Oliveira <rioliveira@bionexo.com>
2023-01-26 19:28:28 -0300

    Show clock numbers before task selected

--- src/components/chronometer/index.tsx
+++ src/components/chronometer/index.tsx
@@ -1,13 +1,26 @@
+import { useEffect, useState } from 'react';
+import { tempoParaSegundos } from '../../common/utils/time';
+import { ITarefa } from '../../types/tarefas';
 import Botao from '../button';
 import Relogio from './clock'
 import style from './Cronometro.module.scss';
 
-export default function Cronometro() {
+interface Props {
+    selecionado: ITarefa | undefined
+}
+export default function Cronometro({selecionado} : Props) {
+    const [tempo, setTempo] = useState<number>();
+
+    useEffect(() => {
+        if(selecionado?.tempo) {
+            setTempo(tempoParaSegundos(selecionado?.tempo))
+        }
+    }, [selecionado]);
     return (
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
             <div className={style.relogioWrapper}>
-                <Relogio/>
+                <Relogio tempo={tempo}/>
             </div>
             <Botao>
                 Começar!
--- src/components/list/item/index.tsx
+++ src/components/list/item/index.tsx
@@ -1,5 +1,5 @@
 import { ITarefa } from '../../../types/tarefas'
-import style from '../List.module.scss'
+import style from './Item.module.scss'
 
 interface Props extends ITarefa {
      selecionaTarefa: (tarefaSelecionada: ITarefa) => void
